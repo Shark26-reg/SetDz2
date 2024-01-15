f50f417738a8818dcbe11de4199bae899aa8cd22
Ivannikov Roman <shark71@inbox.ru>
2024-01-15 20:42:16 +0300

    Добавьте файлы проекта.

---
+++ Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SetDz2
+{
+    internal class Message
+    {
+        public string UserName { get; set; }
+        public string Text { get; set; }
+
+        public DateTime DateTime { get; set; }
+        public Message(string u, string text)
+        {
+            UserName = u;
+            Text = text;
+            DateTime = DateTime.Now;
+        }
+        public Message() { }
+
+        public string toJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public static Message? fromJson(string jsonData)
+        {
+            return JsonSerializer.Deserialize<Message>(jsonData);
+
+        }
+        public override string ToString()
+        {
+            return String.Format($"{this.DateTime.ToString()} - {this.UserName}:{this.Text}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SetDz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Server.AcceptMsg();
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    new Thread(() =>
+                    {
+                        Client.SendMsg($"{args[0]} {i}");
+                    }).Start();
+
+                }
+
+            }
+
+
+        }
+    }
+}
